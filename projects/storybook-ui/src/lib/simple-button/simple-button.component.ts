9304a6f19bddfe7c62c06f06fbdc5d58428b7765
Klaus Kazlauskas <klausmachado@gmail.com>
2019-12-31 14:54:41 -0300

    feat(simple-button): adicione um input de texto

--- projects/storybook-ui/src/lib/simple-button/simple-button.component.ts
+++ projects/storybook-ui/src/lib/simple-button/simple-button.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-simple-button',
   template: `
-    <button>botão</button>
+    <button>{{text}}</button>
   `,
   styles: [``]
 })
 export class SimpleButtonComponent {
+
+  @Input() text = 'clique aqui';
 }
