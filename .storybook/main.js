74b9f2cf3fd18cbc0868342f435971310fc324df
Klaus Kazlauskas <klausmachado@gmail.com>
2020-01-14 21:16:01 -0300

    refactor(storybook): cria o arquivo main.js
    Substitui a necessidade de ter o addons.js e o config.js

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    '../src/**/*.stories.(ts|js)',
+    '../projects/**/*.stories.(ts|js)'
+  ],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions'
+  ]
+};
