f53f9246864b84903a42ce4281c2911f565d28c0
Klaus Kazlauskas <klausmachado@gmail.com>
2020-01-14 21:16:01 -0300

    refactor(storybook): cria o arquivo main.js
    Substitui a necessidade de ter o addons.js e o config.js

--- .storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure } from '@storybook/angular';
-
-configure([
-  require.context('../src', true, /\.stories\.[tj]s$/),
-  require.context('../projects', true, /\.stories\.[tj]s$/)
-], module);
