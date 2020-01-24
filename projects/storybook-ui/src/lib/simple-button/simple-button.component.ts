a8197b1f4e5a014b379805af1efc272dc189b63f
Klaus Kazlauskas <klausmachado@gmail.com>
2020-01-23 22:45:38 -0300

    docs(simple-button): adiciona uma documentação de exemplo
    (cherry picked from commit 9984778a046c936d069ac8351d6e85759a4233fc)

--- projects/storybook-ui/src/lib/simple-button/simple-button.component.ts
+++ projects/storybook-ui/src/lib/simple-button/simple-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ui-simple-button',
@@ -9,5 +9,35 @@ import { Component, Input } from '@angular/core';
 })
 export class SimpleButtonComponent {
 
-  @Input() text = 'clique aqui';
+  /**
+   * ViewChild que está vinculada a nada
+   */
+  @ViewChild('empty', { static: false })
+  empty: TemplateRef<any>;
+
+  /**
+   * Output fake
+   */
+  @Output()
+  clicked = new EventEmitter();
+
+  /**
+   * Input que define o texto apresentado no botão
+   */
+  @Input()
+  text = 'clique aqui';
+
+  /**
+   * Método simples e público
+   * @return void
+   */
+  public simpleMethod(): void {}
+
+  /**
+   * Método complexo e privado
+   * @return string
+   */
+  private complexMethod(num1: number, num2: number): string {
+    return '';
+  }
 }
