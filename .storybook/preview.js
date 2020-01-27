5a3d14c9495791cc0fc4486b70bea7becb8692ce
Klaus Kazlauskas <klaus.machado@totvs.com.br>
2020-01-27 10:12:26 -0300

    refactor(design-token): configura os parametros no preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,11 +8,33 @@ setCompodocJson(docJson);
 
 addDecorator(withKnobs);
 
+const contextPath = '!!raw-loader!../projects/storybook-ui/src/lib/design-system';
+
+const createTokenFiles = (path, deep, filter) => {
+  const requireContext = require.context(path, deep, filter);
+  return requireContext
+    .keys()
+    .map(filename => ({ filename, content: requireContext(filename).default }));
+};
+
+const cssTokenFiles = createTokenFiles(contextPath, true, /.\.css$/);
+const scssTokenFiles = createTokenFiles(contextPath, true, /.\.scss$/);
+const lessTokenFiles = createTokenFiles(contextPath, true, /.\.less$/);
+const svgIconTokenFiles = createTokenFiles(contextPath, true, /.\.svg$/);
+
 addParameters({
   options: {
     showRoots: true,
   },
   docs: {
     iframeHeight: 60
-  }
+  },
+  designToken: {
+    files: {
+      css: cssTokenFiles,
+      scss: scssTokenFiles,
+      less: lessTokenFiles,
+      svgIcons: svgIconTokenFiles
+    }
+  },
 });
